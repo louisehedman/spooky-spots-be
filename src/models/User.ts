0ab63798dbcfcde797b6fbec5c6f7343ff6cbf13
louisehedman <louise.hedman@chasacademy.se>
2022-09-01 12:15:11 +0200

    create user interface and model

---
+++ src/models/User.ts
@@ -0,0 +1,64 @@
+import mongoose, { Schema, Types } from "mongoose";
+import bcrypt from 'bcryptjs';
+
+interface IUser {
+    isModified(arg: string): any;
+    matchPassword(password: string): boolean | PromiseLike<boolean>;
+    resetPasswordToken: string|undefined;
+    resetPasswordExpire: string|undefined;
+    username: string;
+    avatar?: string;
+    password: string;
+    email: string;
+    role: number;
+}
+
+const UserSchema: Schema = new Schema<IUser>({
+    username: {
+        type: String,
+        uniqe: true,
+        required: [true, "Can't be blank"],
+        index: true,
+    },
+    avatar: {
+        type: String,
+        default: "",
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+        minlength: [8, "Please use minimum of 8 characters"],
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: [true, "Can't be blank"],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please use a valid email address'],
+        unique: true,
+        index: true
+    }, 
+    role: {
+        type: Number, 
+        default: 0,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: String,
+});
+
+UserSchema.pre<IUser>("save", async function (next: any) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = bcrypt.hashSync(this.password, salt); 
+    next();
+});
+
+UserSchema.methods.matchPassword = async function (password:string) {
+    return await bcrypt.compare(password,this.password)   
+}
+
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
