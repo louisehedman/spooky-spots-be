ebf7aec05c88f8ce5e1b2f891b7b47fdcc55473d
louisehedman <louise.hedman@chasacademy.se>
2022-09-14 20:27:48 +0200

    add cookie-parser middleware and implement listEndpoints to show existing routes in browser

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
-import express from 'express';
+import express, { Request, Response } from 'express';
+import router from "./routes/router";
+import cookieParser from 'cookie-parser';
+import listEndpoints from 'express-list-endpoints';
 import { connectDb } from "./database/connection";
 
 const app = express();
@@ -7,9 +10,18 @@ const app = express();
 const PORT = process.env.PORT;
 const dbURI = process.env.DB_URI;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", router);
+
+router.get('/', (req: Request, res: Response) => {
+  res.send(listEndpoints(app))
+})
+
+
+/*app.get('/', (req, res) => {
   res.send('Hello World!');
-});
+});*/
 
 app.listen(PORT, () => {
   return console.log(`Express is listening at http://localhost:${PORT}`);
