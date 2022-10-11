c808a994067a123d955a520b173305b79d044597
louisehedman <louise.hedman@chasacademy.se>
2022-10-11 10:17:22 +0200

    update ICommunitySubject with threads

--- src/models/CommunitySubject.ts
+++ src/models/CommunitySubject.ts
@@ -1,23 +1,27 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
+import { ICommunityThread } from "./CommunityThread";
 
 // Interface for CommunitySubject
 export interface ICommunitySubject extends Document {
   title: string;
-};
+  threads: Types.DocumentArray<ICommunityThread>;
+}
 
-const CommunitySubjectSchema: Schema = new Schema(
+const CommunitySubjectSchema: Schema = new Schema({
+  title: {
+    type: String,
+  },
+  threads: [
     {
-        title: {
-            type: String
-        },
-        threads: [
-            {
-              type: Schema.Types.ObjectId,
-              ref: 'CommunityThread',
-            },
-          ],
-    });
+      type: Schema.Types.ObjectId,
+      ref: "CommunityThread",
+    },
+  ],
+});
 
-    const CommunitySubject = mongoose.model<ICommunitySubject>("CommunitySubject", CommunitySubjectSchema);
+const CommunitySubject = mongoose.model<ICommunitySubject>(
+  "CommunitySubject",
+  CommunitySubjectSchema
+);
 
 export default CommunitySubject;
